040b9943076b14e78a315858951411c24773f3a2
Matteo Miceli <omatteomiceli@gmail.com>
2022-01-24 02:13:35 -0800

    getting operations array from diff

--- client/app.js
+++ client/app.js
@@ -12,60 +12,11 @@ socket.on("joined", (msg) => {
 });
 
 function getDiff(prev, curr) {
-    if (prev.length < curr.length) {
-        let indexBreak;
-        for (let i = 0; i < curr.length; i++) {
-            const currChar = curr[i];
-            const prevChar = prev[i];
-            if (currChar != prevChar) {
-                indexBreak = i;
-                console.log(prev, curr);
-                break;
-            }
-        }
-        curr.substring();
-    }
+    const diff = Diff.diffChars(prev, curr);
+    console.log(diff);
 }
-console.log(Diff);
-editor.addEventListener("click", () => {
-    if (editor.selectionStart == editor.selectionEnd) {
-        //console.log(editor.selectionStart);
-    }
-});
 
-// naive solution -- sending data each update
-// editor.addEventListener("keyup", (e) => {
-//     const data = e.target.value;
-//     console.log(e.key);
-//     socket.emit("update", data);
-// });
-
-// positional solution (theoretically scales bette)
-// editor.addEventListener("keydown", (e) => {
-//     if (editor.selectionStart === editor.selectionEnd && e.key.length == 1) {
-//         socket.emit("insert", { pos: editor.selectionStart, char: e.key });
-//     }
-// });
-
-// editor.addEventListener("keydown", (e) => {
-//     console.log(e.key)
-//     if (editor.selectionStart === editor.selectionEnd && e.key === "Backspace") {
-//         socket.emit("delete", editor.selectionStart)
-//     } else if (editor.selectionStart === editor.selectionEnd && e.key === "Enter") {
-//         socket.emit("insert", { pos: editor.selectionStart, char: "\n" });
-//     }
-// })
 let data = "";
-
-// let prev;
-// editor.addEventListener("keydown", (e) => {
-//     prev = e.target.value
-// })
-// editor.addEventListener('keyup', (e) => {
-//     if (prev !== e.target.value) {
-//         getDiff(prev, e.target.value)
-//     }
-// })
 
 editor.addEventListener("input", (e) => {
     getDiff(data, e.currentTarget.value);
